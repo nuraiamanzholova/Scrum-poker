5fbc1c9b64b69c437a28f584a7f37a7cda7857dc
nuraiamanzholova <amanzholova@publicplan.de>
2025-11-14 19:56:53 +0100

    feat/B6PD-85 . home page added

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <>
+            <h1> Home page </h1>
+            <h3>
+                {' '}
+                gehe auf http://localhost:5173/login <br /> oder <br />{' '}
+                http://localhost:5173/room <br />{' '}
+            </h3>
+        </>
+    )
+}

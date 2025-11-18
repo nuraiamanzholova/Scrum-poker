dac6c10734d5d65c74ee1ba31f965514b3753651
nuraiamanzholova <amanzholova@publicplan.de>
2025-11-18 17:19:23 +0100

    zwischenstand

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,10 @@
+import CreateRoomButton from '../components/creatroom'
 export default function Home() {
     return (
         <>
             <h1> Home page </h1>
             <h1>
-                <button> Einen Raum erstellen</button>
+                <CreateRoomButton />
             </h1>
         </>
     )

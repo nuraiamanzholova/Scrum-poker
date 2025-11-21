3d225bc5b3f57562ea0f19e3a6fe4a28edaf7f6f
nuraiamanzholova <amanzholova@publicplan.de>
2025-11-21 10:40:45 +0100

    --

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,8 @@
-import CreateRoomButton from '../components/creatroom'
 export default function Home() {
     return (
         <>
             <h1> Home page </h1>
-            <h1>
-                <CreateRoomButton />
-            </h1>
+            <h1></h1>
         </>
     )
 }
